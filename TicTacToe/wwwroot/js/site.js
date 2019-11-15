6352048049976fc89a8c1140840630c4b5121d45
Jason Byrne <jjbyrne@ksu.edu>
2019-11-15 17:07:05 -0600

    checkers added

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -52,3 +52,33 @@ function checkForWin() {
 
     return false;
 }
+
+var squares = document.querySelectorAll("#checkers-board > .square");
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('dragenter', onDragEnter);
+    squares[i].addEventListener('dragleave', onDragLeave);
+    squares[i].addEventListener('dragstart', onDragStart);
+    squares[i].addEventListener('dragend', onDragEnd);
+}
+
+function onDragEnter(event) {
+    if (event.target.ClassList.contains("checker")) return;
+    if (event.target.ClassList.contains("red")) return;
+    if (event.target.children.length > 0) return;
+    event.target.style.backgroundColor = "gold";
+    document.getElementById("toX").value = event.target.dataset.x;
+    document.getElementById("toY").value = event.target.dataset.y;
+}
+
+function onDragLeave(event) {
+    event.target.style.backgroundColor = null;
+}
+
+function onDragStart(event) {
+    document.getElementById("fromX").value = event.target.dataset.x;
+    document.getElementById("fromY").value = event.target.dataset.y;
+}
+
+function onDragEnd(event) {
+    document.getElementByName("checkers-form").submit();
+}
